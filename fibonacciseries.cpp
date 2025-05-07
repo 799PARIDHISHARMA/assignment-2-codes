1d010eb580ab75408a89ee0155a0c279821ac2ee
799PARIDHISHARMA <paridhis529@gmail.com>
2025-05-07 16:27:03 +0530

    Update fibonacciseries.cpp

--- fibonacciseries.cpp
+++ fibonacciseries.cpp
@@ -1,13 +1,26 @@
- #include<iostream>
- using namespace std;
- int main(){
- int num, term ;
- cin>>num;
- cin>>term;
- for(int i=0; i<=term ;i++){
-      i = num + i;
-      num= num + i;
- }
- cout<<num<<endl;
- return 0;
+#include <iostream>
+using namespace std;
+int main() {
+    int terms;
+    cout << "Enter the number of terms: ";
+    cin >> terms;
+
+    int first = 0, second = 1, next;
+
+    if (terms <= 0) {
+        cout << "Please enter a positive number of terms." << endl;
+    } else if (terms == 1) {
+        cout << first << endl;
+    } else {
+        cout << first << " " << second;
+        for (int i = 3; i <= terms; i++) {
+            next = first + second;
+            cout << " " << next;
+            first = second;
+            second = next;
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
